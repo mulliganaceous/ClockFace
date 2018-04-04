358a0f954dacbcbbee19a4829d33d3cda5df1c12
IJsbrand S. Mulligan <mulliganaceous@gmail.com>
2018-04-03 20:15:34 -0400

    Fixed leap year.

--- main/TimeDate.java
+++ main/TimeDate.java
@@ -106,7 +106,12 @@ public class TimeDate {
 	}
 	
 	public static boolean isLeapYear(int year) {
-		return year % 4 == 0 && year % 100 != 0;
+		if (year % 4 != 0) return false;
+		else if (year % 4 == 0) {
+			if (year % 400 == 0) return true;
+			else if (year % 100 == 0 && year % 400 != 0) return false;
+			else return true;
+		}
 	}
 	
 	public String toString() {
